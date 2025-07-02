93cb7f6bcf80d3175374f649fb3a16eea02b3e22
Tejas Kanazriya <101577007+Tejaskt@users.noreply.github.com>
2025-07-02 17:54:53 +0530

    Add files via upload

---
+++ DAY1/createReact/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    <h1>Tejas kanzariya</h1>
+    </>
+  );
+}
+
+export default App;
+
